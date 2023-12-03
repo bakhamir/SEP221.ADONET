b1798ae3f7e5fd370fb61f37f7c1f38d4eddc6fa
БахытжановА <@214-2>
2023-12-03 17:01:47 +0600

    Добавьте файлы проекта.

---
+++ MyConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Data.SqlClient;
+
+namespace MyConsole
+{
+    internal class Program
+    {
+         static string connectionstring1 = "Server=214-2;Database=testdb;Trusted_Connection=True";
+         static string connectionstring2 = "";
+        static void testconnection()
+        {
+            using (SqlConnection db = new SqlConnection(connectionstring1))
+            {
+                db.Open();
+                Console.WriteLine(db.State.ToString());
+                db.Close();
+                Console.WriteLine(db.State.ToString());
+            }
+        }
+        static void Main(string[] args)
+        {
+            testconnection();
+            Console.WriteLine("Hello, World!");
+        }
+
+    }
+}
