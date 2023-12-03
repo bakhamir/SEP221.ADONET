daae1cc9a49d15e7f35feee48e1d1db6d050df23
БахытжановА <@214-2>
2023-12-03 17:22:36 +0600

    конец первой пары в академии

--- MyConsole/Program.cs
+++ MyConsole/Program.cs
@@ -5,7 +5,7 @@ namespace MyConsole
     internal class Program
     {
          static string connectionstring1 = "Server=214-2;Database=testdb;Trusted_Connection=True";
-         static string connectionstring2 = "";
+         static string connectionstring2 = "Server=214-2;Database=testdb;User Id=user1;Password=111;";
         static void testconnection()
         {
             using (SqlConnection db = new SqlConnection(connectionstring1))
@@ -16,9 +16,36 @@ namespace MyConsole
                 Console.WriteLine(db.State.ToString());
             }
         }
+        static void testconnection1()
+        {
+            using (SqlConnection db = new SqlConnection(connectionstring2))
+            {
+                db.Open();
+                Console.WriteLine(db.State.ToString());
+                db.Close();
+                Console.WriteLine(db.State.ToString());
+            }
+        }
+
+        static void getdate()
+        {
+            using (SqlConnection db = new SqlConnection(connectionstring1))
+            {
+                db.Open();
+                using(SqlCommand cmd = new SqlCommand("Select GETDATE()", db))
+                {
+                    Console.WriteLine(cmd);
+                }
+                Console.WriteLine();
+                db.Close();
+            }
+        }
+
         static void Main(string[] args)
         {
-            testconnection();
+            //testconnection();
+            //testconnection1();
+            getdate();
             Console.WriteLine("Hello, World!");
         }
 
